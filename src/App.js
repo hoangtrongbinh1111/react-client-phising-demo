aa7969562d523c4566255940935f45a02ca9a3cc
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2022-12-25 23:07:36 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -5,65 +5,48 @@ import { io } from "socket.io-client";
 
 function App() {
   const socket = useRef();
+  const [labData, setLabData] = useState("173b6a00-f818-4b13-a92e-445bd984503c");
 
   useEffect(() => {
-    socket.current = io("http://0.0.0.0:6789");
+    socket.current = io("https://c2d8-117-4-240-104.ngrok.io");
 
     socket.current.on("connect", () => {
       console.log("connected to server");
     });
 
-
-    socket.current.on('send_preprocess_result_261599',(data)=>{
-      console.log((121221,data));
-    })
-    
-
-    socket.current.on("send_training_result_261599", (data) => {
+    socket.current.on(`send_training_result_${labData}`, (data) => {
       console.log(121221, data);
     });
   
-    socket.current.on('send_testing_result_261599',(data) => {
+    socket.current.on(`send_testing_result_${labData}`,(data) => {
       console.log(data);
     })
 
-    socket.current.on('send_infering_result_261599',(data) => {
+    socket.current.on(`send_infering_result_${labData}`,(data) => {
       console.log(data);
     })
   }, []);
- 
-
-
-  const handlePreprocess = () => {
-    socket.current.emit('start_preprocess_model',{
-      sid: '261599',
-      datasetId: 'a6cc7a6d-1556-46da-8b73-60d4a3dd1896',
-      // labId: '2a414760-fbb5-4374-962d-c29bee9730ad'
-    })
-  }
 
   const handleClickTrain = () => {
+    // setLabData(document.getElementById("labElement").value);
     socket.current.emit("start_train_model", {
-      sid: "261599",
-      datasetId: 'a6cc7a6d-1556-46da-8b73-60d4a3dd1896',
+      datasetId: document.getElementById("datasetElement").value,
       labId: document.getElementById("labElement").value,
     });
   };
 
-
-
   const handleClickTest = () => {
+    // setLabData(document.getElementById("labElement").value);
     socket.current.emit('start_test_model',{
-      sid: "261599",
       labId : document.getElementById("labElement").value,
       epoch_selected: document.getElementById("epochElement").value,
-      datasetId: 'a6cc7a6d-1556-46da-8b73-60d4a3dd1896'
+      datasetId: document.getElementById("datasetElement").value
     })
   }
 
   const handleClickInfer = () => {
+    // setLabData(document.getElementById("labElement").value);
     socket.current.emit('start_infer_model',{
-      sid: "261599",
       labId : document.getElementById("labElement").value,
       epoch_selected: document.getElementById("epochElement").value
     })
@@ -74,14 +57,11 @@ function App() {
     <div className="App">
       <p>Socket.io app</p>
       <input type="text" id="labElement" placeholder="Lab Id" />
+      <input type="text" id="datasetElement" placeholder="Dataset Id" />
       <input type="text" id="epochElement" placeholder="Epoch" />
 
-      <button type="button" onClick={handlePreprocess}>
-        Preeee
-      </button>
-
       <button type="button" onClick={handleClickTrain} style={{marginLeft: 5 + 'px'}}>
-        Emit a time message
+        Train
       </button>
 
       <button type = "button" onClick= {handleClickTest} style={{marginLeft: 5 + 'px'}}>
