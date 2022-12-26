51e42d21671567d3120a606aa0e02d06c3b00661
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2022-12-26 08:04:53 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [labData, setLabData] = useState("173b6a00-f818-4b13-a92e-445bd984503c");
 
   useEffect(() => {
-    socket.current = io("https://c2d8-117-4-240-104.ngrok.io");
+    socket.current = io("http://c2d8-117-4-240-104.ngrok.io");
 
     socket.current.on("connect", () => {
       console.log("connected to server");
@@ -55,7 +55,12 @@ function App() {
  
   return (
     <div className="App">
-      <p>Socket.io app</p>
+      <p>Demo training lab</p>
+      <ul>
+        <li>Step 1: Training - Nhap ID của bài Lab - Sau đó ấn training - Bật Log của Dev Tools lên để xem kết quả trả về, thời gian sẽ phải chờ đợi từ 2-5 phút</li>
+        <li>Step 2: Testing - Vẫn giữ nguyên Id của bài Lab - Nhập epoch để test thử mô hình tương ứng với checkpoint epoch đó - Kết qủa được trả ra ở Log của Dev Tools</li>
+        <li>Step 3: Inference - Nhập đường dẫn url xong Infer để xem kết quả ở Log của Dev Tools</li>
+      </ul>
       <input type="text" id="labElement" placeholder="Lab Id" />
       <input type="text" id="datasetElement" placeholder="Dataset Id" />
       <input type="text" id="epochElement" placeholder="Epoch" />
@@ -65,11 +70,11 @@ function App() {
       </button>
 
       <button type = "button" onClick= {handleClickTest} style={{marginLeft: 5 + 'px'}}>
-        Choosen best epoch and test
+        Test
       </button>
 
       <button type= "button" onClick={handleClickInfer} style= {{marginLeft: 5 + 'px'}}>
-        INFERRRRRRR
+        Infer
       </button>
 
     
